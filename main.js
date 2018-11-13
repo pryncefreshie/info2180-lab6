b021be2a9a44e8e7394946db356d8d1af37565fa
Ackeem Smith <ackeem.smith@mymona.uwi.edu>
2018-11-13 21:46:00 +0000

    Exercise 1

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onload = main;
+ function ajax_query(word){
+    let httpRequest = new XMLHttpRequest();
+    let url = "/request.php?q="+word
+    
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState === XMLHttpRequest.DONE ){
+            if(httpRequest.status === 200){
+                let response = httpRequest.responseText;
+                alert(response);
+            }else{
+                alert("There was some error");
+            }
+        }
+    }
+    
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+ function main(){
+    let submit = $("#submission")[0];
+    submit.onclick = function(event){
+        event.preventDefault();
+        ajax_query("definition");
+    };
+    
+} 
